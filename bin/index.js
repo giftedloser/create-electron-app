53358709f81cc1b75257a73b9a063632866a29b4
Marshall <128746330+giftedloser@users.noreply.github.com>
2025-06-30 16:23:34 -0600

    chore: restore cli executable flag

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 // File: bin/index.js
 
+import gradient from "gradient-string";
 import { createAppWizard } from "../src/wizard.js";
 import { scaffoldProject } from "../src/generator.js";
 import { showSummaryReport } from "../src/report.js";
 import { info, error as logError } from "../src/utils/logger.js";
+
+const title = `
+                             __                          .__                 __                                                       
+  ___________   ____ _____ _/  |_  ____             ____ |  |   ____   _____/  |________  ____   ____           _____  ______ ______  
+_/ ___\\_  __ \\_/ __ \\__  \\   __\\/ __ \\   ______ _/ __ \\|  | _/ __ \\_/ ___\\   __\\_  __ \\/  _ \\ /    \\   ______ \\__  \\ \____ \\____ \\ 
+\\  \___|  | \\/\\  ___/ / __ \\|  | \\  ___/  /_____/ \\  ___/|  |_\\  ___/\\  \___|  |  |  | \\(  <_> )   |  \\ /_____/  / __ \\|  |_> >  |_> >
+ \___  >__|    \\___  >____  /__|  \\___  >          \\___  >____/\\___  >\\___  >__|  |__|   \\____/|___|  /         (____  /   __/|   __/ 
+     \\/            \\/     \\/          \\/               \\/          \\/     \\/                        \\/               \\/|__|   |__|    
+`;
+
+console.clear();
+console.log(gradient.rainbow.multiline(title));
 
 async function main() {
   try {
@@ -24,3 +37,4 @@ async function main() {
 }
 
 main();
+
