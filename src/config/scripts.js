de060771411cbd98835b7f409c1a24093dc38371
Marshall <128746330+giftedloser@users.noreply.github.com>
2025-06-30 16:43:31 -0600

    feat: add cross-env for dev script

--- src/config/scripts.js
+++ src/config/scripts.js
@@ -13,7 +13,7 @@ export const scriptOptions = [
 ];
 
 export const fullScriptMap = {
-  dev: "NODE_ENV=development vite --config vite.config.js && NODE_ENV=development electron .",
+  dev: "cross-env NODE_ENV=development vite --config vite.config.js && cross-env NODE_ENV=development electron .",
   build: "tsc && vite build",
   dist: "electron-builder",
   clean: "rimraf dist build .cache",
