2ddd5583f171faad2a50e1c295b09327f5b5181d
Marshall <128746330+giftedloser@users.noreply.github.com>
2025-07-01 12:34:40 -0600

    extend CSSProperties for frameless template

--- templates/with-frameless/src/global.d.ts
+++ templates/with-frameless/src/global.d.ts
@@ -1,7 +1,15 @@
+import 'react';
+
 export interface WindowControls {
   minimize: () => void;
   maximize: () => void;
   close: () => void;
+}
+
+declare module 'react' {
+  interface CSSProperties {
+    WebkitAppRegion?: string;
+  }
 }
 
 declare global {
